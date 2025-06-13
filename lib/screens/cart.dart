15e4b50f8442f0b22215e323949249fc33f0e50a
Edgardo Armoa <armoanet@gmail.com>
2025-06-13 13:27:53 -0300

    Se actualizo el card.dart

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -6,23 +6,12 @@ import 'package:pharma/provider/auth_provider.dart';
 import 'package:pharma/provider/cart_provider.dart';
 import 'package:pharma/screens/login.dart';
 import 'package:pharma/screens/order_confirmation_screen.dart';
+import 'package:pharma/services/functions.dart';
 // import 'package:pharma/screens/order_confirmation_screen.dart';
 
 import 'package:provider/provider.dart';
 
 final numeroFormat = NumberFormat("#,###", "es_PY");
-
-String numberFormat(String x) {
-  List<String> parts = x.toString().split('.');
-  RegExp re = RegExp(r'\B(?=(\d{3})+(?!\d))');
-  parts[0] = parts[0].replaceAll(re, '.');
-  if (parts.length == 1) {
-    parts.add('');
-  } else {
-    parts[1] = parts[1].padRight(2, '0').substring(0, 2);
-  }
-  return parts.join('');
-}
 
 class CartScreenView extends StatelessWidget {
   const CartScreenView({super.key});
